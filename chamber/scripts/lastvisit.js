d16e547433184c20433c383e201207db171a3011
mjsherwood <mjsherwood@gmail.com>
2023-02-17 15:52:55 -0800

    Fixed last visit

--- chamber/scripts/lastvisit.js
+++ chamber/scripts/lastvisit.js
@@ -1,16 +1,16 @@
 // Get the last visit time from local storage
 let lastVisit = localStorage.getItem('lastVisit');
 
-// If there is no last visit time in local storage, set it to the current time
+// If there is no last visit time in local storage, set it to the current time and display "First Visit"
 if (!lastVisit) {
   lastVisit = new Date().getTime();
   localStorage.setItem('lastVisit', lastVisit);
+  document.getElementById('last-visit').textContent = "First Visit";
+} else {
+  // Calculate the number of days since the last visit
+  const ONE_DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
+  const daysSinceLastVisit = Math.round((new Date().getTime() - lastVisit) / ONE_DAY_IN_MILLISECONDS);
+
+  // Update the div with the number of days since the last visit
+  document.getElementById('last-visit').textContent = `${daysSinceLastVisit}`;
 }
-
-// Calculate the number of days since the last visit
-const ONE_DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
-const daysSinceLastVisit = Math.round((new Date().getTime() - lastVisit) / ONE_DAY_IN_MILLISECONDS);
-
-// Update the div with the number of days since the last visit
-const lastVisitDiv = document.getElementById('last-visit');
-lastVisitDiv.textContent = `${daysSinceLastVisit}`;
