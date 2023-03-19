410c290222455735bc5a79e3eadee144b1a9f027
mjsherwood <mjsherwood@gmail.com>
2023-03-18 19:19:04 -0700

    Changes to Directory page.

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -4,43 +4,55 @@ async function getBusinessData(url) {
     const response = await fetch(url);
     const data = await response.json();
     displayBusinesses(data.businesses);
-  }
+}
 
-  getBusinessData(url);
+getBusinessData(url);
 
-  const displayBusinesses = (businesses) => {
-    const cards = document.querySelector('div.cards'); // select the output container element
+const displayBusinesses = (businesses) => {
+    const cards = document.querySelector('div.cards');
+    const gridButton = document.getElementById('grid');
+    const listButton = document.getElementById('list');
 
     businesses.forEach((business) => {
-      // Create elements to add to the div.cards element
-      let card = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let icon = document.createElement('img');
-      let phone = document.createElement('p');
-      let weburl = document.createElement('p');
-      let street = document.createElement('p')
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let icon = document.createElement('img');
+        let phone = document.createElement('p');
+        let weburl = document.createElement('p');
+        let street = document.createElement('p')
 
-      // Build the h2 content out to show the business name - finish the template string
-      h2.textContent = `${business.name}`;
-      phone.textContent = `Phone: ${business.phone}`;
-      weburl.textContent = `${business.weburl}`;
-      street.textContent = `Street: ${business.street}`
+        h2.textContent = `${business.name}`;
+        phone.textContent = `Phone: ${business.phone}`;
+        weburl.textContent = `${business.weburl}`;
+        street.textContent = `Street: ${business.street}`
 
+        icon.setAttribute('src', business.image);
+        icon.setAttribute('alt', `${business.name} Logo`);
+        icon.setAttribute('loading', 'lazy');
+        icon.setAttribute('width', 'auto');
+        icon.setAttribute('height', '100');
 
-      // Build the image portrait by setting all the relevant attribute
-      icon.setAttribute('src', business.image);
-      icon.setAttribute('alt', `${business.name} Logo`);
-      icon.setAttribute('loading', 'lazy');
-      icon.setAttribute('width', 'auto');
-      icon.setAttribute('height', '100');
+        card.appendChild(h2);
+        card.appendChild(icon);
+        card.appendChild(weburl);
+        card.appendChild(phone);
+        card.appendChild(street);
+        cards.appendChild(card);
+    });
 
+    gridButton.addEventListener('click', () => {
+        cards.classList.remove('list-view');
+        cards.querySelectorAll('section').forEach(card => card.classList.remove('list'));
+        cards.querySelectorAll('img').forEach(img => img.classList.remove('list'));
+        cards.querySelectorAll('h2').forEach(h2 => h2.classList.remove('list'));
+        cards.querySelectorAll('p').forEach(p => p.classList.remove('list'));
+    });
 
-      // Append the section(card) with the created elements
-      card.appendChild(h2);
-      card.appendChild(icon);
-      card.appendChild(weburl);
-      card.appendChild(phone);
-      card.appendChild(street);
-      cards.appendChild(card);
-    }) // end of forEach loop
-  } // end of function expression
+    listButton.addEventListener('click', () => {
+        cards.classList.add('list-view');
+        cards.querySelectorAll('section').forEach(card => card.classList.add('list'));
+        cards.querySelectorAll('img').forEach(img => img.classList.add('list'));
+        cards.querySelectorAll('h2').forEach(h2 => h2.classList.add('list'));
+        cards.querySelectorAll('p').forEach(p => p.classList.add('list'));
+    });
+}
